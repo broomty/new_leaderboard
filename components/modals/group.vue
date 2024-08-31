4027533e62f96b5e41837b22f7d4bc40db49285a
kato emma <emmanuelkato39@gmail.com>
2024-08-31 16:18:00 +0300

    handled species error

--- components/modals/group.vue
+++ components/modals/group.vue
@@ -34,7 +34,7 @@ const closeModal = () => {
                 <h3 class="dark:text-gray-300">Ranked:{{ parish.rank }} / {{ store.rankedParishes.length }} in {{
                     store.activity ==
                         'Potted' ? 'Potting' : store.activity == 'Pricked' ? 'pricking' : store.activity ==
-                            'Sorted' ? 'sorting' :'Distribution'}}</h3>
+                            'Sorted' ? 'sorting' : 'Distribution' }}</h3>
                 <button @click="closeModal" class="text-gray-400 hover:text-gray-600 dark:hover:text-gray-300">
                     &times;
                 </button>
@@ -44,8 +44,8 @@ const closeModal = () => {
             <div class="mt-4 text-gray-600 dark:text-gray-300">
                 <p class="font-bold">Parish Coordinator: <span class="font-normal">{{ parish.coordinator }}</span></p>
 
-                <p class="mt-2 font-bold">Branch: <span class="font-normal">{{ parish.branch ?
-                    parish.branch.split('|')[0] : 'No Branch' }}</span></p>
+                <p class="mt-2 font-bold">Branch: <span class="font-normal">{{ parish.branch !== '' ?
+                    parish.branch : 'No Branch' }}</span></p>
 
                 <!-- Scrollable container for groups -->
                 <div class="grid grid-cols-3 overflow-auto h-64 2xl:h-96">
@@ -54,18 +54,18 @@ const closeModal = () => {
                         <div class="flex justify-between">
                             <p class="font-bold text-sm">{{ groupKey }}</p>
                             <div class="group ">
-                            <i class="fa fa-ellipsis-v"></i>
-                            <div class="hidden absolute inset-0 flex items-center justify-center group-hover:block">
-                                <div class="bg-green-400 p-4 text-sm rounded-lg text-white">
-                                    <h2 class="font-bold">SPECIES:</h2>
-                                    <p v-for="specie in group.species.split(',')" :key="specie">
-                                        {{ specie }}
-                                    </p>
+                                <i class="fa fa-ellipsis-v"></i>
+                                <div class="hidden absolute inset-0 flex items-center justify-center group-hover:block">
+                                    <div class="bg-green-400 p-4 text-sm rounded-lg text-white">
+                                        <h2 class="font-bold">SPECIES:</h2>
+                                        <p v-for="specie in (group.species || '').split(',')" :key="specie">
+                                            {{ specie }}
+                                        </p>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                        </div>
-                        
+
                         <p class="text-sm">Number of visits: {{ group.visits }}</p>
 
                         <!-- Centered Species Card -->
