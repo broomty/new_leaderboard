3aff466087a8ce5f8dd38c8ded3e1060df0acb4f
katoemma <emmanuelkato39@gmail.com>
2024-09-12 14:10:08 +0300

    updated the middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,13 +1,13 @@
-// middleware/auth.js
-
 export default defineNuxtRouteMiddleware((to, from) => {
-    const token = useCookie('token');
-  
-    // Example: Redirect if no token is found
-    if (!token.value) {
-      return navigateTo('/auth/login');
-    }else if (token.value !=='' && to.path === '/auth/login') {
-      return navigateTo('/');
-    }
-  });
-  
+  const token = useCookie('token');
+
+  // Redirect to login if no token is found
+  if (!token.value && to.path !== '/auth/login') {
+    return navigateTo('/auth/login');
+  }
+
+  // Prevent logged-in users from accessing the login page again
+  if (token.value && to.path === '/auth/login') {
+    return navigateTo('/');
+  }
+});
