1254c737efb8ea5132929a5df6205eaec6868cab
katoemma <emmanuelkato39@gmail.com>
2024-12-03 13:18:28 +0300

    added enter key

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -4,8 +4,8 @@ const colorMode = useColorMode();
 colorMode.preference = 'light';
 
 definePageMeta({
-    middleware: 'auth',
-    colorMode: 'light',
+  middleware: 'auth',
+  colorMode: 'light',
 });
 
 useSeoMeta({
@@ -13,18 +13,22 @@ useSeoMeta({
   description: 'Login with your email to view parish Performance',
   ogTitle: 'Login - Kijani leaderboard',
   ogDescription: 'Login with your email to view parish Performance',
-})
+});
 
 const serverUrl = 'https://leaderboard-auth.vercel.app';
-//const serverUrl =  process.env.API_SERVER_URL ||'http://localhost:5000';
+//const serverUrl = process.env.API_SERVER_URL || 'http://localhost:5000';
 
 const verifying = ref(false);
-
 const errMsg = ref('');
 const email = ref('');
 
-const login = async () => {
+// Function to validate email with regex
+const validateEmail = (email) => {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+};
 
+const login = async () => {
   // Validate email
   const validEmail = validateEmail(email.value);
   if (email.value === '' || !validEmail) {
@@ -48,12 +52,10 @@ const login = async () => {
       const data = await response.json();
       const tokenCookie = useCookie('token');
       tokenCookie.value = data.token;
-      //simulate a delay to show the loading spinner
       await new Promise((resolve) => setTimeout(resolve, 1000));
 
-      // Wait for the cookie to be set and then fetch data
       await store.fetchData();
-      store.addUserData(data.user.userData)
+      store.addUserData(data.user.userData);
 
       // Navigate to dashboard
       navigateTo('/');
@@ -68,45 +70,18 @@ const login = async () => {
   }
 };
 
-
-//function to validate email with regex
-const validateEmail = (email) => {
-  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
-}
-
-//function to toggle colorMode
-// const toggleColorMode = () => {
-//   if (colorMode.preference === 'system') {
-//     if(colorMode.value === 'dark') {
-//       colorMode.preference = 'light';
-//     } else {
-//       colorMode.preference = 'dark';
-//     }
-//   } else {
-//     colorMode.preference = 'system'
-//   }
-// }
-
-
+// Trigger login function when Enter key is pressed
+const handleKeyPress = (event) => {
+  if (event.key === 'Enter') {
+    login();
+  }
+};
 </script>
 
 <template>
   <div class="flex justify-end p-8 px-12 absolute right-0 top-0 w-full">
-    <!-- <button @click="toggleColorMode"
-      class="text-white font-bold dark:bg-gray-800 p-3 px-6 rounded-2xl hover:bg-gray-200 dark-hover:bg-gray-100 transition-colors duration-300 ease-in-out">
-      <span v-if="colorMode.value === 'dark'" class="flex items-center gap-4">
-        <i class="fa fa-sun-o text-xl"></i>
-        <span>Light Mode</span>
-
-      </span>
-      <span v-else class="flex items-center gap-4 text-black">
-        <i class="fa fa-moon-o text-xl"></i>
-        <span>Dark Mode</span>
-      </span>
-    </button> -->
+    <!-- Optional button for toggling dark/light mode -->
   </div>
-
 
   <div class="bg-gray-100 dark:bg-gray-900 flex flex-col items-center justify-center min-h-screen px-4 sm:px-6 lg:px-8">
     <!-- logo -->
@@ -120,14 +95,14 @@ const validateEmail = (email) => {
       <div v-if="errMsg != ''" class="border border-red-500 p-2 m- w-full md:w-96 rounded-lg bg-red-100 dark:bg-gray-800">
         <p class="text-red-500">{{ errMsg }}</p>
       </div>
-      <form action="#" method="POST" @submit.prevent>
+      <form @submit.prevent="login" @keydown="handleKeyPress">
         <div class="mb-4">
           <label for="email" class="block text-gray-700 dark:text-gray-300 font-medium mb-2">Email</label>
           <input type="email" id="email" name="email" v-model="email"
             class="w-full p-2 sm:p-3 border border-gray-300 dark:border-gray-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 dark:focus:ring-blue-400"
             placeholder="Enter your email" required />
         </div>
-        <button type="button" @click="login"
+        <button type="submit"
           class="w-full bg-[#265E3C] dark:bg-green-600 text-white py-2 sm:py-3 rounded-lg font-semibold hover:bg-green-700 dark:hover:bg-green-700 transition duration-200">
           {{ verifying ? 'verifying..... ' : 'Login' }} <i class="fa fa-gear animate-spin" v-if="verifying"></i>
         </button>
